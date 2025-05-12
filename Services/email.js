17d0c79c1e05e746675672773ddfeb093a65cfab
vishalgohil2201 <vishalgohil2201@gmail.com>
2025-05-12 18:44:45 +0530

    Fixed a changes

--- Services/email.js
+++ Services/email.js
@@ -1,11 +1,10 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_NAME,
-    pass: process.env.APP_PASS
+    user: `${process.env.EMAIL_NAME}`,
+    pass: `${process.env.APP_PASS}`
   }
 });
 
@@ -18,7 +17,12 @@ const sendEmail = async (to, subject, text, html) => {
     html
   };
 
-  return transporter.sendMail(mailOptions);
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+  } catch (error) {
+    console.error("Error sending email: " + error);
+  }
 };
 
 module.exports = sendEmail;
